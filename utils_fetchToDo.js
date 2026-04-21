4ead84c4949b9b0b3013a8777377225be334ddf2
Azola Keliwe <azolakeliwe12@gmail.com>
2026-04-21 04:23:29 +0200

    Implement CLI logic with dynamic TODO ID from CLI argument

--- utils_fetchToDo.js
+++ utils_fetchToDo.js
@@ -1,5 +1,5 @@
 export const fetchTodo = async (id) => {
- const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+ const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`) //No longer Hard coded
   const data = await response.json()
   return data
 }
